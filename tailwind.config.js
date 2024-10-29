547723eb69b2667cfcaa7bf9694ad59d2084b079
Murilo Henrique Lodi <132021915+MuriloLodi@users.noreply.github.com>
2024-10-29 12:38:01 -0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
         "home": "url('/img/fundoimg.jpg')",
         "video": "url('/img/cafeteria-1920w.png')",
         "img1": "url('/img/coffee.jpeg')"
+        
       }
     },
   },
